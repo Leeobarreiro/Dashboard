24cb943561eba5b647a77dfa72e413bd82356adb
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2024-04-19 15:28:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
     <div className="app">
-
+      <main className="content"></main>
     </div>
+    </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
