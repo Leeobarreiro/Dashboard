2164615682b874b9e3bf7d787b7e5c9c618cdb45
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2024-04-19 08:25:47 -0300

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -5,7 +5,7 @@ export const mockDataTeam = [
     id: 1,
     name: "Jon Snow",
     email: "jonsnow@gmail.com",
-    age: 35,
+    age: 35,    
     phone: "(665)121-5454",
     access: "admin",
   },
